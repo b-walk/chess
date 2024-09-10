c6ae22aa13443026f30d4b1cd45d8e761b7d18da
Blake <blameswalker@gmail.com>
2024-09-10 06:47:12 -0700

    Wrap Square class within the Chess module

--- lib/chess/square.rb
+++ lib/chess/square.rb
@@ -1,9 +1,11 @@
 #frozen_string_literal: true
 
-class Square
-  def initialize(rank, file, piece = nil)
-    @rank = rank
-    @file = file
-    @piece = piece
+module Chess
+  class Square
+    def initialize(rank, file, piece = nil)
+      @rank = rank
+      @file = file
+      @piece = piece
+    end
   end
 end
